f4983e0a5b2599a9a342ddde74380058b1213100
Rati <og.mogorosi28@gmail.com>
2024-03-14 19:46:26 +0200

    php file change

--- php/login.php
+++ php/login.php
@@ -2,4 +2,8 @@
 
 require('dbconnect.php');
 
+$email = $_POST['email'];
+$password = $_POST['password'];
+$confirmedpassword = $_POST['confirmedpassword'];
+
 ?>
--- php/signup.php
+++ php/signup.php
@@ -4,6 +4,8 @@ require('dbconnect.php');
 
 if ($_SERVER['REQUEST_METHOD']) {
     //GET User input
+    $first_name = $_POST['first_name'];
+    $surname = $_POST['surname'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $confirmedpassword = $_POST['confirmedpassword'];
