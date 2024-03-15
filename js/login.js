553666ea50b1c3f22a8a746d55342d58a58d3464
Rati <og.mogorosi28@gmail.com>
2024-03-15 18:40:33 +0200

    form pop-up

--- js/login.js
+++ js/login.js
@@ -1,11 +1,13 @@
 const openLogin = document.querySelector('.user-action-btn[aria-label="user"');
-const formContainer = document.getElementsByClassName('.form_container');
-const closeLogin = document.getElementsByClassName('.form_close');
+const formContainer = document.querySelector('.form_container');
+const closeLogin = document.querySelector('.form_close');
 const userLogin = document.querySelector('.button');
+
 
 //user icon 
 function userLoginClick() {
     console.log("user btn clicked");
+    formContainer.style.display = formContainer.style.display === 'none' ? 'block' : 'none';
     
 }
 
