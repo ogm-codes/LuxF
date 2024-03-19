d761a74f7e02a8d23737bcb877377aa9d647f9f5
Rati <og.mogorosi28@gmail.com>
2024-03-19 15:32:09 +0200

    toggle between signup and login

--- js/login.js
+++ js/login.js
@@ -2,10 +2,11 @@ const openLogin = document.querySelector('.user-action-btn[aria-label="user"');
 const formContainer = document.querySelector('.form_container');
 const closeLogin = document.querySelector('.form_close');
 const signuplink = document.querySelector('#signup');
+const loginLink = document.querySelector('#login');
 const userLogin = document.querySelector('.loginbutton');
 const userSignup = document.querySelector('.signupbutton');
 const loginForm = document.querySelector('.login_form');
-const signupForm = document.querySelector();
+const signupForm = document.querySelector('signup_form');
 
 
 //user icon 
@@ -16,16 +17,21 @@ function userLoginClick() {
 }
 
 function closeForm() {
-//Fix
     console.log("close form");
     formContainer.style.display = 'none';
 }
 
 function signupnewuser() {
     console.log("sign up a new user");
+    loginForm.style.display = 'none';
+    signupForm.style.display = 'block';
+}
 
+function loginExistingUser() {
+    console.log("login link clicked");
 }
 
 openLogin.addEventListener('click', userLoginClick);
 closeLogin.addEventListener('click', closeForm);
 signuplink.addEventListener('click', signupnewuser);
+loginLink.addEventListener('click', loginExistingUser);
